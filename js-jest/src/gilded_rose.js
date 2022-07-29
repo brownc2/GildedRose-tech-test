cd614438c750b499009b635aac08c63b8397c9d9
Christopher <christopher2brown@hotmail.co.uk>
2022-07-29 13:54:26 +0100

    added further class logic  and tests

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -9,10 +9,16 @@ class Item {
 
 class Standard extends Item {
   updateItem() {
+    this.sellIn -= 1;
+    this.quality -= 1;
     if (this.sellIn <= 0) {
-      this.quality -= 2;
-    } else {
-      (this.sellIn -= 1), (this.quality -= 1);
+      this.quality -= 1;
+    }
+    if (this.quality <= 0) {
+      this.quality = 0;
+    }
+    if (this.quality >= 50) {
+      this.quality = 50;
     }
   }
 }
@@ -20,12 +26,24 @@ class Standard extends Item {
 class AgedBrie extends Item {
   updateItem() {
     (this.sellIn -= 1), (this.quality += 1);
+    if (this.quality >= 50) {
+      this.quality = 50;
+    }
   }
 }
 
 class Backstage extends Item {
   updateItem() {
     (this.sellIn -= 1), (this.quality += 1);
+    if (this.sellIn <= 10) {
+      this.quality += 1;
+    }
+    if (this.sellIn <= 5) {
+      this.quality += 1;
+    }
+    if (this.quality >= 50) {
+      this.quality = 50;
+    }
   }
 }
 class Conjured extends Item {
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -15,19 +15,19 @@ describe("Gilded Rose", function () {
   });
 
   describe("Will work out the sellIn and quality of an item after one day", () => {
-    xit("Will correctly work out +5 Dexterity Vest after one day", () => {
+    it("Will correctly work out +5 Dexterity Vest after one day", () => {
       const gildedRose = new Shop([new Standard("+5 Dexterity Vest", 10, 20)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(9);
       expect(items[0].quality).toBe(19);
     });
-    xit("Will correctly work out Aged Brie after one day", () => {
+    it("Will correctly work out Aged Brie after one day", () => {
       const gildedRose = new Shop([new AgedBrie("Aged Brie", 2, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(1);
       expect(items[0].quality).toBe(1);
     });
-    xit("Will correctly work out Elixir of the Mongoose after one day", () => {
+    it("Will correctly work out Elixir of the Mongoose after one day", () => {
       const gildedRose = new Shop([
         new Standard("Elixir of the Mongoose", 5, 7),
       ]);
@@ -35,7 +35,7 @@ describe("Gilded Rose", function () {
       expect(items[0].sellIn).toBe(4);
       expect(items[0].quality).toBe(6);
     });
-    xit("Will correctly work out Sulfuras, Hand of Ragnaros (1st test) after one day", () => {
+    it("Will correctly work out Sulfuras, Hand of Ragnaros (1st test) after one day", () => {
       const gildedRose = new Shop([
         new Item("Sulfuras, Hand of Ragnaros", 0, 80),
         new Item("+5 Dexterity Vest", 10, 20),
@@ -44,7 +44,7 @@ describe("Gilded Rose", function () {
       expect(items[0].sellIn).toBe(0);
       expect(items[0].quality).toBe(80);
     });
-    xit("Will correctly work out Sulfuras, Hand of Ragnaros (2nd test) after one day", () => {
+    it("Will correctly work out Sulfuras, Hand of Ragnaros (2nd test) after one day", () => {
       const gildedRose = new Shop([
         new Item("Sulfuras, Hand of Ragnaros", -1, 80),
       ]);
@@ -52,7 +52,7 @@ describe("Gilded Rose", function () {
       expect(items[0].sellIn).toBe(-1);
       expect(items[0].quality).toBe(80);
     });
-    xit("Will correctly work out Backstage passes to a TAFKAL80ETC concert (1st test) after one day", () => {
+    it("Will correctly work out Backstage passes to a TAFKAL80ETC concert (1st test) after one day", () => {
       const gildedRose = new Shop([
         new Backstage("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       ]);
@@ -61,7 +61,7 @@ describe("Gilded Rose", function () {
       expect(items[0].quality).toBe(21);
     });
 
-    xit("Will correctly work out Conjured Mana Cake after one day", () => {
+    it("Will correctly work out Conjured Mana Cake after one day", () => {
       const gildedRose = new Shop([new Conjured("Conjured Mana Cake", 3, 6)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(2);
@@ -71,8 +71,51 @@ describe("Gilded Rose", function () {
       const gildedRose = new Shop([new Standard("+5 Dexterity Vest", 0, 20)]);
       const items = gildedRose.updateQuality();
       console.log(items);
-      expect(items[0].sellIn).toBe(0);
+      expect(items[0].sellIn).toBe(-1);
       expect(items[0].quality).toBe(18);
+    });
+    it("will no show a negative number for +5 Dexterity Vest if quality hits 0 ", () => {
+      const gildedRose = new Shop([new Standard("+5 Dexterity Vest", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      console.log(items);
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(0);
+    });
+    it("Will correctly work out Aged Brie if quality hits 50", () => {
+      const gildedRose = new Shop([new AgedBrie("Aged Brie", 20, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(19);
+      expect(items[0].quality).toBe(50);
+    });
+    it("Will correctly work out +5 Dexterity Vest if quality hits 50", () => {
+      const gildedRose = new Shop([new Standard("+5 Dexterity Vest", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(-1);
+      expect(items[0].quality).toBe(0);
+    });
+    it("Will correctly work out Backstage passes to a TAFKAL80ETC concert if sellin is less than 10", () => {
+      const gildedRose = new Shop([
+        new Backstage("Backstage passes to a TAFKAL80ETC concert", 9, 20),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(8);
+      expect(items[0].quality).toBe(22);
+    });
+    it("Will correctly work out Backstage passes to a TAFKAL80ETC concert if sellin is less than 5", () => {
+      const gildedRose = new Shop([
+        new Backstage("Backstage passes to a TAFKAL80ETC concert", 4, 20),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(3);
+      expect(items[0].quality).toBe(23);
+    });
+    it("Will correctly work out Backstage passes to a TAFKAL80ETC concert if quality is 50", () => {
+      const gildedRose = new Shop([
+        new Backstage("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(3);
+      expect(items[0].quality).toBe(50);
     });
   });
 });
